211545bdc31876b5b5222d0a3f3d886c2fb0ddbd
mutazs21 <140478630+mutazs21@users.noreply.github.com>
2025-01-06 09:48:50 +0700

    Add files via upload

---
+++ kalender.py
@@ -0,0 +1,58 @@
+import calendar
+
+while True: 
+    
+    bulan = int(input(f'Enter the month number you want to view (example: 1 = January)'))
+    
+    if bulan == 1:
+        yy = 2025  # year
+        mm = 1     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 2:       
+        yy = 2025  # year
+        mm = 2     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 3:       
+        yy = 2025  # year
+        mm = 3     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 4:       
+        yy = 2025  # year
+        mm = 4     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 5:       
+        yy = 2025  # year
+        mm = 5     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 6:       
+        yy = 2025  # year
+        mm = 6     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 7:       
+        yy = 2025  # year
+        mm = 7     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 8:       
+        yy = 2025  # year
+        mm = 8     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 9:       
+        yy = 2025  # year
+        mm = 9     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 10:       
+        yy = 2025  # year
+        mm = 10     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 11:       
+        yy = 2025  # year
+        mm = 11     # month
+        print(calendar.month(yy, mm))
+    elif bulan == 12:       
+        yy = 2025  # year
+        mm = 12     # month
+        print(calendar.month(yy, mm))
+    else:
+        True
+
+# display the calendar
